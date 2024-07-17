61cab9f89cadebc04b1c12516b508b9a36b6b103
gziz <gerardo29101@hotmail.com>
2024-07-17 17:48:46 -0600

    Fix vercel deployment

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Server waiting to redirect you to your favorite IDE...!');
+});
 
 app.get('*', (req, res) => {
-  const fullUrl = req.url.slice(1);  // Remove leading slash
+  const fullUrl = req.url.slice(1);
   const vscodePrefix = 'vscode://';
   
   if (fullUrl.startsWith(vscodePrefix)) {
@@ -11,7 +15,7 @@ app.get('*', (req, res) => {
     console.log(`Redirecting to: ${vscodeUrl}`);
     res.redirect(vscodeUrl);
   } else {
-    res.status(400).send('Invalid URL format');
+    res.status(400).send('Invalid VSCode URL format');
   }
 });
 
